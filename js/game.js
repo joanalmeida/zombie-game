1e9aed47bca64af455ad30d4820a3b0eb334ff97
Joan <joan.almeida90@gmail.com>
2016-07-14 16:01:59 -0300

    Adding game files

---
+++ js/game.js
@@ -0,0 +1,40 @@
+var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update});
+
+var player;
+
+var cursors;
+
+function preload() {
+	game.load.spritesheet('character', 'assets/player.png', 95, 95, 11);
+
+}
+
+function create() {
+	game.physics.startSystem(Phaser.Physics.ARCADE);
+
+	player = game.add.sprite(400, 300, 'character');
+	game.physics.arcade.enable(player);
+	player.anchor.setTo(0.5, 0.5);
+	player.body.collideWorldBounds = true;
+	player.animations.add('move', [4, 5, 6, 8, 9, 10, 3, 7], 10, true);
+	player.frame = 2;
+
+	cursors = game.input.keyboard.createCursorKeys();
+}
+
+function update() {
+	player.body.velocity.x = 0;
+
+	if(cursors.right.isDown){
+		player.scale.x = 1;
+		player.body.velocity.x = 150;
+		player.animations.play('move');
+	} else if(cursors.left.isDown){
+		player.scale.x = -1;
+		player.body.velocity.x = -150;
+		player.animations.play('move');
+	} else {
+		player.animations.stop();
+		player.frame = 2;
+	}
+}
